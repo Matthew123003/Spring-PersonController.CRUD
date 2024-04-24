4f9eb3c1ddc12549962db185eeab4c1caf54d35a
Matt Kramer <matthew123003@gmail.com>
2024-04-24 13:18:37 -0400

    Finished

--- src/main/java/io/zipcoder/crudapp/PersonService.java
+++ src/main/java/io/zipcoder/crudapp/PersonService.java
@@ -2,6 +2,7 @@ package io.zipcoder.crudapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class PersonService {
@@ -17,7 +18,7 @@ public class PersonService {
     }
 
     public Person show(Long id) {
-        return repository.findById(id).get();
+        return repository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     public Person create(Person person) {
@@ -25,7 +26,7 @@ public class PersonService {
     }
 
     public Person update(Long id, Person newPersonData) {
-        Person originalPerson = repository.findById(id).get();
+        Person originalPerson = repository.findById(id).orElseThrow(EntityNotFoundException::new);
         originalPerson.setfName(newPersonData.getfName());
         originalPerson.setlName(newPersonData.getlName());
         return repository.save(originalPerson);
